a964b61ead0a811d01e0cf6fe4e05f351955d157
Connor White <connorwhitepdx@gmail.com>
2022-11-29 13:00:42 -0800

    menu structured and styled

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import gsap from 'gsap';
+import { NavLink } from 'react-router-dom';
 import './index.css';
 
 function Menu () {
     return (
         <div className='menu'>
-            
+            <div className='marquee'>
+                <div className='ticker'>
+                    CONNOR WHITE - INTERACTIVE DESIGNER - DEVELOPER - 3D ARTIST - AUGMENTED REALITY - REALTIME 3D - ANIMATION - ADDITIVE MANUFACTURING
+                </div>
+            </div>
+            <div className='nav'>
+                <div>
+                    <NavLink to="/" className="nav-link">work</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/info" className="nav-link">info</NavLink>
+                </div>
+            </div>
         </div>
     );
 }
