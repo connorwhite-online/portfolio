9f1348430b07bf6830c1b1ec28dfaba7ded0cbc4
Connor White <connorwhitepdx@gmail.com>
2022-11-30 17:14:20 -0800

    menu animations locked

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import gsap from 'gsap';
 import { NavLink } from 'react-router-dom';
 import './index.css';
 
 function Menu () {
+
+    const menuRef = useRef();
+
+    useEffect(() => {
+        let ctx = gsap.context(() => {
+            gsap.fromTo('.nav, .marquee', {
+                scaleX: 0,
+            }, {
+                scaleX: 1,
+                duration: 1,
+                ease: 'power4.inOut'
+            });
+            gsap.fromTo('.nav-link', {
+                opacity: 0,
+                scaleY: -50,
+            }, {
+                opacity: 1,
+                scaleY: 0,
+                duration: 1,
+                delay: 1,
+                ease: 'power3.inOut',
+            });
+            gsap.fromTo('.ticker', {
+                opacity: 0,
+                x: 50,
+            }, {
+                opacity: 1,
+                x: 0,
+                duration: 1,
+                delay: 1.5,
+                ease: 'power3.inOut',
+            });
+        }, menuRef)
+    }, []);
+
     return (
-        <div className='menu'>
+        <div className='menu' ref={menuRef}>
             <div className='marquee'>
                 <div className='ticker'>
                     CONNOR WHITE - INTERACTIVE DESIGNER - DEVELOPER - 3D ARTIST - AUGMENTED REALITY - REALTIME 3D - ANIMATION - ADDITIVE MANUFACTURING
