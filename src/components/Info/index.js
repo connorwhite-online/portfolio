710c0f16d2d90e10c6a927037e4d2373d249765b
Connor White <connorwhitepdx@gmail.com>
2022-12-05 22:30:18 -0800

    copy written, links linked, style's a mess

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -5,7 +5,16 @@ import './index.css';
 function Info () {
     return (
         <div className='info'>
-            
+            <div className='bio'>
+                Hi, I go by Connor.<br />Thanks for taking the time to check out some of my work. I’m a creative technologist primarily interested in real-time 3D applications like augmented reality and visual communication for e-commerce. I believe in technology as a means of improving the human experience, not distracting from it.
+            </div>
+            <div className='link-group'>
+                <div className='link-box'><a href="mailto:connorwhitepdx@gmail.com" className='link'>E-MAIL</a></div>
+                <div className='link-box'><a href="https://www.github.com/connorwhite-online" target="_blank" rel="noopener noreferrer" className='link'>GITHUB</a></div>
+                <div className='link-box'><a href="https://www.linkedin.com/in/connorwhite-online/" target="_blank" rel="noopener noreferrer" className='link'>LINKEDIN</a></div>
+                <div className='link-box'><a href="https://www.instagram.com/connorwhite.online" target="_blank" rel="noopener noreferrer" className='link'>INSTAGRAM</a></div>
+                <div className='link-box'><a href="https://www.twitter.com/connor_online" target="_blank" rel="noopener noreferrer" className='link'>TWITTER</a></div>
+            </div>
         </div>
     );
 }
