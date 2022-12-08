7f0337209642d6ba684da7615c9ab2572973594a
Connor White <connorwhitepdx@gmail.com>
2022-12-07 16:26:14 -0800

    info page animated

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { startTransition, useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import './index.css';
 
 function Info () {
 
+    const infoRef = useRef();
+
+    // Page Loading Animation
+    useEffect(() => {
+
+        const boxes = gsap.utils.toArray('.link-box');
+        const links = gsap.utils.toArray('.link');
+        let ctx = gsap.context(() => {
+            gsap.fromTo('.bio, .link-group', {
+                scaleX: 0,
+            }, {
+                scaleX: 1,
+                duration: 1,
+                ease: 'power4.inOut',
+            });
+            gsap.fromTo('h1', {
+                opacity: 0,
+            }, {
+                opacity: 1,
+                duration: 1,
+                delay: .5,
+                ease: 'power3.inOut',
+            });
+            boxes.forEach((box, i) => {
+                gsap.fromTo(box, {
+                    scaleX: 0,
+                }, {
+                    scaleX: 1,
+                    duration: 1,
+                    delay: i*.25,
+                    ease: 'power4.inOut',
+                })
+            });
+            links.forEach((link, i) => {
+                gsap.fromTo(link, {
+                    opacity: 0,
+                    y: 50,
+                }, {
+                    opacity: 1,
+                    y: 0,
+                    duration: 1,
+                    delay: i*.25,
+                    ease: 'power4.inOut',
+                })
+            });
+        }, infoRef)
+        return () => ctx.revert();
+    }, []);
+
+
     return (
-        <div className='info'>
+        <div className='info' ref={infoRef}>
             <div className='bio'>
-                Hi, I go by Connor.<br />Thanks for taking the time to check out some of my work. I’m a creative technologist primarily interested in real-time 3D applications like augmented reality and visual communication for e-commerce. I believe in technology as a means of improving the human experience, not distracting from it.
+                <h1>
+                    Hi, I go by Connor.<br />Thanks for taking the time to check out some of my work. I’m a creative technologist primarily interested in real-time 3D applications like augmented reality and visual communication for e-commerce. I believe in technology as a means of improving the human experience, not distracting from it.
+                </h1>
             </div>
             <div className='link-group'>
                 <div className='link-box'><a href="mailto:connorwhitepdx@gmail.com" className='link'>E-MAIL</a></div>
